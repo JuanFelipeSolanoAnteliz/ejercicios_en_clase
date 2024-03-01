380235ce8062c1cd842e7cff939e476a03b543f9
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-01 09:32:29 -0500

    circulos

---
+++ programas_simples/circulos.py
@@ -0,0 +1,6 @@
+radio = float(input("ingrese el radio de su circulo: "))
+
+area = 3.1416 * radio**2
+perimetro = 3.1418 * radio * 2
+
+print(f"""el are de su circulo es {area} y su perimetro o valor de circunferencia es {perimetro}""")
