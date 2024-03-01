486f720d80ae6d108ee57299e20bbec7ab0eaa15
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-01 13:21:40 -0500

    promedio de notas

---
+++ programas_simples/promedio.py
@@ -0,0 +1,8 @@
+print("Ingrese tres valores para promediarlos")
+num1 = float(input("Ingrese su primera nota: "))
+num2 = float(input("Ingrese su segunda nota: "))
+num3 = float(input("Ingrese su tercera nota: "))
+
+promedio = (num1 + num2 + num3)/ 3
+
+print(f"Su nota promedio es: {promedio}.")
