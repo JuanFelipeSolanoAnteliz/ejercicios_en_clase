9d1533ce6e5cab83be01300ae13dbed57319c18a
JuanFelipeSolanoAnteliz <160759957+JuanFelipeSolanoAnteliz@users.noreply.github.com>
2024-03-05 14:05:00 -0500

    hora futura

---
+++ programas_simples/horaFutura.py
@@ -0,0 +1,13 @@
+hora_actual_str = input("Ingrese la hora actual (formato HH:MM): ") 
+
+hora_actual_lista = hora_actual_str.split(":")
+hora_actual = int(hora_actual_lista[0])
+minutos_actual = int(hora_actual_lista[1])
+
+horas_sumar = int(input("Ingrese el numero de horas a sumar:  "))
+
+hora_futura = (hora_actual+horas_sumar) % 24
+
+minutos_futuro = (minutos_actual+(horas_sumar*60)) % 60 
+
+print(f"la hora despues de {horas_sumar} sera: {hora_futura:02d}:{minutos_futuro:02d}")
